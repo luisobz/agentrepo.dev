a4a5f872a15f8bdd78bacd918310c21921420df2
Luis Ballester Zafra <luis@agentrepo.dev>
2026-05-29 19:17:53 +0200

    chore: setup infra

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -5,6 +5,7 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: 'standalone',
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
